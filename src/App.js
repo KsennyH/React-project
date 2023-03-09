35b3aa5806b29fcd660653d14e6f694f48b5c347
Ksenny <ksenny_h@mail.ru>
2023-03-09 21:08:22 +0300

    products-list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import Card from './components/card/Card';
 import './css/reset.css';
 import './css/base.css';
+import './css/main.css';
 import { cards } from './data/cards';
 
 
 function App() {
   return (
     <div className="App">
-      {cards.map((el) => {
-        return (
-          <Card title={el.title} img={el.img} reserve={el.reserve} price={el.price} />
-        )
-      })}
+      <div className='products'>
+        <div className='products__title'>
+          <h2 className='title'>Телескопы</h2>
+        </div>
+        <ul className='products__list'>
+          {cards.map((el) => {
+            return (
+              <li className='products__item'>
+                <Card title={el.title} img={el.img} reserve={el.reserve} price={el.price} />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
