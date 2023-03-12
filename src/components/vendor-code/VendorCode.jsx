a48acb5f1173c8f4781c6fd0e6aa96eddfb2ba27
Ksenny <ksenny_h@mail.ru>
2023-03-12 20:09:45 +0300

    component VendorCode

---
+++ src/components/vendor-code/VendorCode.jsx
@@ -0,0 +1,12 @@
+import './VendorCode.css';
+
+const VendorCode = () => {
+    return (
+        <div className="vendor-code">
+            <p className="vendor-code__number">Артикул: 56412</p>
+            <p className="vendor-code__reserve">В наличии</p>
+        </div>
+    )
+}
+
+export default VendorCode;
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,6 +1,7 @@
 import Rating from '../../components/card/rating/Rating';
 import Price from '../../components/price/Price';
 import ProductImg from '../../components/product-img/ProductImg';
+import VendorCode from '../../components/vendor-code/VendorCode';
 import './Product.css';
 
 const Product = () => {
@@ -9,6 +10,7 @@ const Product = () => {
             <ProductImg />
             <Rating />
             <Price />
+            <VendorCode />
         </>
     )
 }
