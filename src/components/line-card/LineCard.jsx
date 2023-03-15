37825d7785c8cf9951692fc9111784118da263ce
Ksenny <ksenny_h@mail.ru>
2023-03-15 21:21:38 +0300

    component LineCard

---
+++ src/components/line-card/LineCard.jsx
@@ -0,0 +1,32 @@
+import './lineCard';
+import { NavLink } from 'react-router-dom';
+import Btn from '../card/button/btn';
+import Rating from '../card/rating/Rating';
+
+const LineCard = (props) => {
+    return (
+        <article className="line-card">
+            <div className="line-card__inner">
+                <div className="line-card__img">
+                    <img src="" alt="" />
+                </div>
+                <NavLink to={`/sale/${props.id}`} className='line-card__link'>
+                    <h3 className='line-card__title'>{props.title}</h3>
+                </NavLink>
+                <div className='line-card__info'>
+                    <Rating feedback={props.feedback} />
+                    <p className='line-card__reserve'>{props.reserve}</p>
+                </div>
+            </div>
+            <div className='line-card__price line-card__price--sale'>
+                <div className="line-card__false-price">{props.falsePrice}</div>
+                <div className="line-card__price-wrapper">
+                    <div className='line-card__price-text'>{props.price}</div>
+                    <Btn text='В корзину' />
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default LineCard;
---
+++ src/pages/sale-line-card/SaleLineCard.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/header/Header';
+import './SaleLineCard.css';
+
+const SaleLineCard = () => {
+    return (
+        <div className="wrapper">
+            <Header />
+            <main className="main">
+                <div className="container">
+                    <section className="products-line-view">
+                        
+                    </section>
+                </div>
+            </main>
+        </div>
+    )
+}
